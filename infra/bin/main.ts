0446ddd682bad6dda2e2cd67c81756fac76e9e89
Matías Mutchinick <mutchinick@gmail.com>
2025-07-01 19:49:38 -0600

    Add DynamoDB and EventBus constructs to infra

--- infra/bin/main.ts
+++ infra/bin/main.ts
@@ -1,17 +1,29 @@
-#!/usr/bin/env node
-import * as cdk from "aws-cdk-lib";
-import { MainStack } from "../lib/MainStack";
+import * as cdk from 'aws-cdk-lib'
+import 'source-map-support/register'
+import { MainStack } from '../lib/MainStack'
 
-const app = new cdk.App();
-new MainStack(app, "MainStack", {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+const deploymentPrefix = process.env.DEPLOYMENT_PREFIX
+if (!deploymentPrefix) {
+  const error = new Error(`Invalid deployment prefix '${deploymentPrefix}'`)
+  console.error(error)
+  throw error
+}
+
+const deploymentStage = process.env.DEPLOYMENT_STAGE
+if (!deploymentStage) {
+  const error = new Error(`Invalid deployment stage '${deploymentStage}'`)
+  console.error(error)
+  throw error
+}
+
+const qualifier = `${deploymentPrefix}-${deploymentStage}`
+const app = new cdk.App()
+new MainStack(app, qualifier, {
+  tags: {
+    qualifier,
+    deploymentPrefix,
+  },
+  config: {
+    deploymentPrefix,
+  },
+})
--- infra/lib/MainStack.ts
+++ infra/lib/MainStack.ts
@@ -1,6 +1,15 @@
 import * as cdk from 'aws-cdk-lib'
+import { Table } from 'aws-cdk-lib/aws-dynamodb'
+import { EventBus } from 'aws-cdk-lib/aws-events'
 import { Construct } from 'constructs'
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { DynamoDbConstruct } from './common/DynamoDbConstruct'
+import { EventBusConstruct } from './common/EventBusConstruct'
+
+export interface IMainStackProps extends cdk.StackProps {
+  config: {
+    deploymentPrefix: string
+  }
+}
 
 /**
  *
@@ -9,14 +18,33 @@ export class MainStack extends cdk.Stack {
   /**
    *
    */
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: IMainStackProps) {
     super(scope, id, props)
 
-    // The code that defines your stack goes here
+    // Common
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { dynamoDbTable, eventBus } = this.createCommon(id)
+  }
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+  /**
+   *
+   */
+  private createCommon(id: string): {
+    dynamoDbTable: Table
+    eventBus: EventBus
+  } {
+    const serviceId = `${id}-Common`
+    const ddbConstructName = `${serviceId}-DynamoDb`
+    const ddbConstruct = new DynamoDbConstruct(this, ddbConstructName)
+
+    const eventBusConstructName = `${serviceId}-EventBus`
+    const eventBusConstruct = new EventBusConstruct(this, eventBusConstructName, {
+      dynamoDbTable: ddbConstruct.dynamoDbTable,
+    })
+
+    return {
+      dynamoDbTable: ddbConstruct.dynamoDbTable,
+      eventBus: eventBusConstruct.eventBus,
+    }
   }
 }

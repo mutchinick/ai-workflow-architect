250215ae4bc348eca0ebdbb598ec25b21acbe215
Matías Mutchinick <mutchinick@gmail.com>
2025-07-06 14:24:41 -0600

    Add EventStoreEventBase class for event handling structure

---
+++ services/src/events/EventStoreEventBase.ts
@@ -0,0 +1,32 @@
+import { FailureKind } from './errors/FailureKind'
+import { Failure, Success } from './errors/Result'
+
+/**
+ *
+ */
+export abstract class EventStoreEventBase {
+  public readonly idempotencyKey: string
+  public readonly eventName: string
+  public readonly eventData: Record<string, unknown>
+  public readonly createdAt: string
+
+  /**
+   *
+   */
+  protected constructor(
+    eventName: string,
+    eventData: Record<string, unknown>,
+    idempotencyKey: string,
+    createdAt: string,
+  ) {
+    this.idempotencyKey = idempotencyKey
+    this.eventName = eventName
+    this.eventData = eventData
+    this.createdAt = createdAt
+  }
+
+  /**
+   *
+   */
+  fromData: (data: Record<string, unknown>) => Success<EventStoreEventBase> | Failure<FailureKind>
+}

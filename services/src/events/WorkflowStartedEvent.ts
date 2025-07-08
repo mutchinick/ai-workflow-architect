1c210953e964f13ace949e76d5a03ab8ccfb6cf4
Matías Mutchinick <mutchinick@gmail.com>
2025-07-08 17:52:21 -0600

    Refactor WorkflowStartedEvent to improve data validation and streamline event creation process

--- services/src/events/WorkflowStartedEvent.ts
+++ services/src/events/WorkflowStartedEvent.ts
@@ -6,15 +6,18 @@ import { EventStoreEventName } from './EventStoreEventName'
 /**
  *
  */
-const schema = z.object({
+const dataSchema = z.object({
   workflowId: z.string().trim().min(6),
   started: z.literal(true),
 })
 
-/**
- *
- */
-export type WorkflowStartedEventData = z.infer<typeof schema>
+export type WorkflowStartedEventData = z.infer<typeof dataSchema>
+
+const eventSchema = z.object({
+  eventData: dataSchema,
+  idempotencyKey: z.string().trim().min(6),
+  createdAt: z.string().datetime(),
+})
 
 /**
  *
@@ -36,9 +39,8 @@ export class WorkflowStartedEvent extends EventStoreEventBase {
     const logCtx = 'WorkflowStartedEvent.fromData'
 
     try {
-      const validData = this.parseValidate(data)
+      const validData = dataSchema.parse(data)
       const idempotencyKey = this.generateIdempotencyKey(validData)
-      // Creates a new event with a fresh timestamp
       const event = new WorkflowStartedEvent(validData, idempotencyKey, new Date().toISOString())
       const eventResult = Result.makeSuccess(event)
       console.info(`${logCtx} exit success:`, { eventResult, data })
@@ -53,45 +55,30 @@ export class WorkflowStartedEvent extends EventStoreEventBase {
   /**
    *
    */
+  private static generateIdempotencyKey(data: WorkflowStartedEventData): string {
+    return `workflow:${data.workflowId}`
+  }
+
+  /**
+   *
+   */
   static reconstitute(
-    data: WorkflowStartedEventData,
+    eventData: WorkflowStartedEventData,
     idempotencyKey: string,
     createdAt: string,
   ): Success<WorkflowStartedEvent> | Failure<'InvalidArgumentsError'> {
     const logCtx = 'WorkflowStartedEvent.reconstitute'
     try {
-      if (!idempotencyKey) {
-        throw new Error('undefined or null idempotencyKey')
-      }
-
-      if (!createdAt) {
-        throw new Error('undefined or null createdAt')
-      }
-
-      const validData = this.parseValidate(data)
-      const event = new WorkflowStartedEvent(validData, idempotencyKey, createdAt)
+      const validEvent = eventSchema.parse({ eventData, idempotencyKey, createdAt })
+      const event = new WorkflowStartedEvent(validEvent.eventData, idempotencyKey, createdAt)
       const eventResult = Result.makeSuccess(event)
-      console.info(`${logCtx} exit success:`, { eventResult, data })
+      console.info(`${logCtx} exit success:`, { eventResult, data: eventData })
       return eventResult
     } catch (error) {
       const failure = Result.makeFailure('InvalidArgumentsError', error, false)
-      console.error(`${logCtx} exit failure:`, { failure, data })
+      console.error(`${logCtx} exit failure:`, { failure, data: eventData })
       return failure
     }
-  }
-
-  /**
-   *
-   */
-  private static parseValidate(data: unknown): WorkflowStartedEventData {
-    return schema.parse(data)
-  }
-
-  /**
-   *
-   */
-  private static generateIdempotencyKey(data: WorkflowStartedEventData): string {
-    return `workflow:${data.workflowId}`
   }
 }
 

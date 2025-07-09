7f7c42d54089506704d85b3ecf19d6ce82f436a3
Matías Mutchinick <mutchinick@gmail.com>
2025-07-08 18:03:26 -0600

    Refactor WorkflowPromptCompletedEvent to improve schema naming, enhance data validation, and streamline event handling

--- services/src/events/WorkflowPromptCompletedEvent.ts
+++ services/src/events/WorkflowPromptCompletedEvent.ts
@@ -6,15 +6,18 @@ import { EventStoreEventName } from './EventStoreEventName'
 /**
  *
  */
-const schema = z.object({
+const dataSchema = z.object({
   workflowId: z.string().trim().min(6),
   objectKey: z.string().trim().min(6),
 })
 
-/**
- *
- */
-export type WorkflowPromptCompletedEventData = z.infer<typeof schema>
+export type WorkflowPromptCompletedEventData = z.infer<typeof dataSchema>
+
+const eventSchema = z.object({
+  eventData: dataSchema,
+  idempotencyKey: z.string().trim().min(6),
+  createdAt: z.string().datetime(),
+})
 
 /**
  *
@@ -33,58 +36,51 @@ export class WorkflowPromptCompletedEvent extends EventStoreEventBase {
    *
    */
   static fromData(
-    data: WorkflowPromptCompletedEventData,
+    eventData: WorkflowPromptCompletedEventData,
   ): Success<WorkflowPromptCompletedEvent> | Failure<'InvalidArgumentsError'> {
     const logCtx = 'WorkflowPromptCompletedEvent.fromData'
 
     try {
-      const validData = this.parseValidate(data)
+      const validData = dataSchema.parse(eventData)
       const idempotencyKey = this.generateIdempotencyKey(validData)
       const event = new WorkflowPromptCompletedEvent(validData, idempotencyKey, new Date().toISOString())
       const eventResult = Result.makeSuccess(event)
-      console.info(`${logCtx} exit success:`, { eventResult, data })
+      console.info(`${logCtx} exit success:`, { eventResult, eventData })
       return eventResult
     } catch (error) {
       const failure = Result.makeFailure('InvalidArgumentsError', error, false)
-      console.error(`${logCtx} exit failure:`, { failure, data })
+      console.error(`${logCtx} exit failure:`, { failure, eventData })
       return failure
     }
+  }
+
+  /**
+   *
+   */
+  private static generateIdempotencyKey(eventData: WorkflowPromptCompletedEventData): string {
+    return `workflowId:${eventData.workflowId}:objectKey:${eventData.objectKey}`
   }
 
   /**
    *
    */
   static reconstitute(
-    data: WorkflowPromptCompletedEventData,
+    eventData: WorkflowPromptCompletedEventData,
     idempotencyKey: string,
     createdAt: string,
   ): Success<WorkflowPromptCompletedEvent> | Failure<'InvalidArgumentsError'> {
     const logCtx = 'WorkflowPromptCompletedEvent.reconstitute'
     try {
-      const validData = this.parseValidate(data)
-      const event = new WorkflowPromptCompletedEvent(validData, idempotencyKey, createdAt)
+      const validEvent = eventSchema.parse(eventData)
+      const event = new WorkflowPromptCompletedEvent(validEvent.eventData, idempotencyKey, createdAt)
       const eventResult = Result.makeSuccess(event)
-      console.info(`${logCtx} exit success:`, { eventResult, data })
+      console.info(`${logCtx} exit success:`, { eventResult, eventData })
       return eventResult
     } catch (error) {
       const failure = Result.makeFailure('InvalidArgumentsError', error, false)
-      console.error(`${logCtx} exit failure:`, { failure, data })
+      console.error(`${logCtx} exit failure:`, { failure, eventData })
       return failure
     }
-  }
-
-  /**
-   *
-   */
-  private static parseValidate(data: unknown): WorkflowPromptCompletedEventData {
-    return schema.parse(data)
-  }
-
-  /**
-   *
-   */
-  private static generateIdempotencyKey(data: WorkflowPromptCompletedEventData): string {
-    return `workflowId:${data.workflowId}:objectKey:${data.objectKey}`
   }
 }
 

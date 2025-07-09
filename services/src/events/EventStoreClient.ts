887881f144bd49dc6f23d670d8a0c27acd54956c
Matías Mutchinick <mutchinick@gmail.com>
2025-07-08 19:47:17 -0600

    Fix: Remove non-null assertion for EVENT_STORE_TABLE_NAME to improve safety

--- services/src/events/EventStoreClient.ts
+++ services/src/events/EventStoreClient.ts
@@ -84,7 +84,7 @@ export class EventStoreClient implements IEventStoreClient {
     // Perhaps we can prevent all errors by validating the arguments, but PutCommand
     // is an external dependency and we don't know what happens internally, so we try-catch
     try {
-      const tableName = process.env.EVENT_STORE_TABLE_NAME!
+      const tableName = process.env.EVENT_STORE_TABLE_NAME
 
       const { eventName, eventData, createdAt, idempotencyKey } = event
 

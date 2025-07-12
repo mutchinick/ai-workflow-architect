650d7c688dadc9f039b69ba8ffc5fd2b69baaa1f
Matías Mutchinick <mutchinick@gmail.com>
2025-07-12 17:31:26 -0600

    Implement SendQueryApiService basic placeholder with some validation logic

---
+++ services/src/workflow/SendQueryApi/SendQueryApiService/SendQueryApiService.test.ts
@@ -0,0 +1,197 @@
+import KSUID from 'ksuid'
+import { FailureKind } from '../../../errors/FailureKind'
+import { Result } from '../../../errors/Result'
+import { IEventStoreClient } from '../../../event-store/EventStoreClient'
+import { TypeUtilsMutable } from '../../../shared/TypeUtils'
+import { WorkflowCreatedEvent, WorkflowCreatedEventData } from '../../events/WorkflowCreatedEvent'
+import { IncomingSendQueryRequest } from '../IncomingSendQueryRequest/IncomingSendQueryRequest'
+import { SendQueryApiService, SendQueryApiServiceOutput } from './SendQueryApiService'
+
+jest.useFakeTimers().setSystemTime(new Date('2024-10-19T03:24:00Z'))
+
+const mockWorkflowId = 'mockWorkflowId'
+const mockQuery = 'mockQuery'
+const mockPromptEnhanceRounds = 3
+const mockResponseEnhanceRounds = 2
+const mockObjectKey = `workflow-${mockWorkflowId}-${new Date().toISOString()}-created`
+
+function buildMockIncomingRequest(): TypeUtilsMutable<IncomingSendQueryRequest> {
+  const mockClass = IncomingSendQueryRequest.fromProps({
+    query: mockQuery,
+    promptEnhanceRounds: 3,
+    responseEnhanceRounds: 2,
+  })
+  return Result.getSuccessValueOrThrow(mockClass)
+}
+
+const mockIncomingRequest = buildMockIncomingRequest()
+
+/*
+ *
+ *
+ ************************************************************
+ * Mock clients
+ ************************************************************/
+function buildMockEventStoreClient_succeeds(value?: unknown): IEventStoreClient {
+  const mockResult = Result.makeSuccess(value)
+  return { publish: jest.fn().mockResolvedValue(mockResult) }
+}
+
+function buildMockEventStoreClient_fails(
+  failureKind?: FailureKind,
+  error?: unknown,
+  transient?: boolean,
+): IEventStoreClient {
+  const mockFailure = Result.makeFailure(
+    failureKind ?? 'UnrecognizedError',
+    error ?? 'UnrecognizedError',
+    transient ?? true,
+  )
+  return { publish: jest.fn().mockResolvedValue(mockFailure) }
+}
+
+describe(`Template Service SendQueryApi SendQueryApiService tests`, () => {
+  /*
+   *
+   *
+   ************************************************************
+   * Test IncomingSendQueryRequest edge cases
+   ************************************************************/
+  it(`does not return a Failure if the input IncomingSendQueryRequest is valid`, async () => {
+    const mockEventStoreClient = buildMockEventStoreClient_succeeds()
+    const sendQueryApiService = new SendQueryApiService(mockEventStoreClient)
+    const result = await sendQueryApiService.sendQuery(mockIncomingRequest)
+    expect(Result.isFailure(result)).toBe(false)
+  })
+
+  it(`returns a non-transient Failure of kind InvalidArgumentsError if the input
+      IncomingSendQueryRequest is undefined`, async () => {
+    const mockEventStoreClient = buildMockEventStoreClient_succeeds()
+    const sendQueryApiService = new SendQueryApiService(mockEventStoreClient)
+    const mockTestRequest = undefined as never
+    const result = await sendQueryApiService.sendQuery(mockTestRequest)
+    expect(Result.isFailure(result)).toBe(true)
+    expect(Result.isFailureOfKind(result, 'InvalidArgumentsError')).toBe(true)
+    expect(Result.isFailureTransient(result)).toBe(false)
+  })
+
+  it(`returns a non-transient Failure of kind InvalidArgumentsError if the input
+      IncomingSendQueryRequest is null`, async () => {
+    const mockEventStoreClient = buildMockEventStoreClient_succeeds()
+    const sendQueryApiService = new SendQueryApiService(mockEventStoreClient)
+    const mockTestRequest = null as never
+    const result = await sendQueryApiService.sendQuery(mockTestRequest)
+    expect(Result.isFailure(result)).toBe(true)
+    expect(Result.isFailureOfKind(result, 'InvalidArgumentsError')).toBe(true)
+    expect(Result.isFailureTransient(result)).toBe(false)
+  })
+
+  it(`returns a non-transient Failure of kind InvalidArgumentsError if the input
+      IncomingSendQueryRequest is not an instance of the class`, async () => {
+    const mockEventStoreClient = buildMockEventStoreClient_succeeds()
+    const sendQueryApiService = new SendQueryApiService(mockEventStoreClient)
+    const mockTestRequest = { ...mockIncomingRequest }
+    const result = await sendQueryApiService.sendQuery(mockTestRequest)
+    expect(Result.isFailure(result)).toBe(true)
+    expect(Result.isFailureOfKind(result, 'InvalidArgumentsError')).toBe(true)
+    expect(Result.isFailureTransient(result)).toBe(false)
+  })
+
+  /*
+   *
+   *
+   ************************************************************
+   * Test internal logic
+   ************************************************************/
+  it(`propagates the Failure if WorkflowCreatedEvent.fromData returns a Failure`, async () => {
+    const mockEventStoreClient = buildMockEventStoreClient_succeeds()
+    const sendQueryApiService = new SendQueryApiService(mockEventStoreClient)
+    const mockFailureKind = 'mockFailureKind' as never
+    const mockError = 'mockError'
+    const mockTransient = 'mockTransient' as never
+    const expectedResult = Result.makeFailure(mockFailureKind, mockError, mockTransient)
+    jest.spyOn(WorkflowCreatedEvent, 'fromData').mockReturnValueOnce(expectedResult)
+    const result = await sendQueryApiService.sendQuery(mockIncomingRequest)
+    expect(Result.isFailure(result)).toBe(true)
+    expect(result).toStrictEqual(expectedResult)
+  })
+
+  it(`calls EventStoreClient.publish a single time`, async () => {
+    const mockEventStoreClient = buildMockEventStoreClient_succeeds()
+    const sendQueryApiService = new SendQueryApiService(mockEventStoreClient)
+    await sendQueryApiService.sendQuery(mockIncomingRequest)
+    expect(mockEventStoreClient.publish).toHaveBeenCalledTimes(1)
+  })
+
+  it(`calls EventStoreClient.publish with the expected input`, async () => {
+    // Mock KSUID to return a fixed workflowId
+    jest.spyOn(KSUID, 'randomSync').mockReturnValue({
+      string: mockWorkflowId,
+    } as never)
+
+    const mockEventStoreClient = buildMockEventStoreClient_succeeds()
+    const sendQueryApiService = new SendQueryApiService(mockEventStoreClient)
+    await sendQueryApiService.sendQuery(mockIncomingRequest)
+    const mockWorkflowCreatedEventInput: WorkflowCreatedEventData = {
+      workflowId: mockWorkflowId,
+      objectKey: mockObjectKey,
+      promptEnhanceRounds: mockPromptEnhanceRounds,
+      responseEnhanceRounds: mockResponseEnhanceRounds,
+    }
+    const expectedWorkflowCreatedEventResult = WorkflowCreatedEvent.fromData(mockWorkflowCreatedEventInput)
+    const expectedWorkflowCreatedEvent = Result.getSuccessValueOrThrow(expectedWorkflowCreatedEventResult)
+    expect(mockEventStoreClient.publish).toHaveBeenCalledWith(expectedWorkflowCreatedEvent)
+  })
+
+  it(`propagates the Failure if EventStoreClient.publish returns a Failure`, async () => {
+    const mockFailureKind = 'mockFailureKind' as never
+    const mockError = 'mockError'
+    const mockTransient = 'mockTransient' as never
+    const mockEventStoreClient = buildMockEventStoreClient_fails(mockFailureKind, mockError, mockTransient)
+    const sendQueryApiService = new SendQueryApiService(mockEventStoreClient)
+    const result = await sendQueryApiService.sendQuery(mockIncomingRequest)
+    const expectedResult = Result.makeFailure(mockFailureKind, mockError, mockTransient)
+    expect(Result.isFailure(result)).toBe(true)
+    expect(result).toStrictEqual(expectedResult)
+  })
+
+  /*
+   *
+   *
+   ************************************************************
+   * Test expected results
+   ************************************************************/
+  it(`returns the expected Success<SendQueryApiServiceOutput> if
+      EventStoreClient.publish returns a Failure of kind DuplicateEventError`, async () => {
+    const mockEventStoreClient = buildMockEventStoreClient_fails('DuplicateEventError')
+    const sendQueryApiService = new SendQueryApiService(mockEventStoreClient)
+    const result = await sendQueryApiService.sendQuery(mockIncomingRequest)
+    const expectedOutput: SendQueryApiServiceOutput = {
+      query: mockIncomingRequest.query,
+      promptEnhanceRounds: mockIncomingRequest.promptEnhanceRounds,
+      responseEnhanceRounds: mockIncomingRequest.responseEnhanceRounds,
+      workflowId: mockWorkflowId,
+      objectKey: mockObjectKey,
+    }
+    const expectedResult = Result.makeSuccess(expectedOutput)
+    expect(Result.isSuccess(result)).toBe(true)
+    expect(result).toStrictEqual(expectedResult)
+  })
+
+  it(`returns the expected Success<SendQueryApiServiceOutput> if the execution path is
+      successful`, async () => {
+    const mockEventStoreClient = buildMockEventStoreClient_succeeds()
+    const sendQueryApiService = new SendQueryApiService(mockEventStoreClient)
+    const result = await sendQueryApiService.sendQuery(mockIncomingRequest)
+    const expectedOutput: SendQueryApiServiceOutput = {
+      query: mockIncomingRequest.query,
+      promptEnhanceRounds: mockIncomingRequest.promptEnhanceRounds,
+      responseEnhanceRounds: mockIncomingRequest.responseEnhanceRounds,
+      workflowId: mockWorkflowId,
+      objectKey: mockObjectKey,
+    }
+    const expectedResult = Result.makeSuccess(expectedOutput)
+    expect(Result.isSuccess(result)).toBe(true)
+    expect(result).toStrictEqual(expectedResult)
+  })
+})
---
+++ services/src/workflow/SendQueryApi/SendQueryApiService/SendQueryApiService.ts
@@ -0,0 +1,121 @@
+import KSUID from 'ksuid'
+import { Failure, Result, Success } from '../../../errors/Result'
+import { IEventStoreClient } from '../../../event-store/EventStoreClient'
+import { TypeUtilsPretty } from '../../../shared/TypeUtils'
+import { WorkflowCreatedEvent, WorkflowCreatedEventData } from '../../events/WorkflowCreatedEvent'
+import { IncomingSendQueryRequest } from '../IncomingSendQueryRequest/IncomingSendQueryRequest'
+
+export interface ISendQueryApiService {
+  sendQuery: (
+    incomingRequest: IncomingSendQueryRequest,
+  ) => Promise<Success<SendQueryApiServiceOutput> | Failure<'InvalidArgumentsError'> | Failure<'UnrecognizedError'>>
+}
+
+export type SendQueryApiServiceOutput = TypeUtilsPretty<
+  IncomingSendQueryRequest & { workflowId: string; objectKey: string }
+>
+
+/**
+ *
+ */
+export class SendQueryApiService implements ISendQueryApiService {
+  /**
+   *
+   */
+  constructor(private readonly eventStoreClient: IEventStoreClient) {}
+
+  /**
+   *
+   */
+  public async sendQuery(
+    incomingRequest: IncomingSendQueryRequest,
+  ): Promise<Success<SendQueryApiServiceOutput> | Failure<'InvalidArgumentsError'> | Failure<'UnrecognizedError'>> {
+    const logCtx = 'SendQueryApiService.sendQuery'
+    console.info(`${logCtx} init:`, { incomingRequest })
+
+    const workflowId = KSUID.randomSync().string
+    const objectKey = `workflow-${workflowId}-${new Date().toISOString()}-created`
+
+    const inputValidationResult = this.validateInput(incomingRequest)
+    if (Result.isFailure(inputValidationResult)) {
+      console.error(`${logCtx} exit failure:`, { inputValidationResult, incomingRequest })
+      return inputValidationResult
+    }
+
+    const publishEventResult = await this.publishWorkflowCreatedEvent(incomingRequest, workflowId, objectKey)
+    if (Result.isSuccess(publishEventResult)) {
+      const serviceOutput: SendQueryApiServiceOutput = { ...incomingRequest, workflowId, objectKey }
+      const serviceOutputResult = Result.makeSuccess(serviceOutput)
+      console.info(`${logCtx} exit success:`, { serviceOutputResult, incomingRequest })
+      return serviceOutputResult
+    }
+
+    if (Result.isFailureOfKind(publishEventResult, 'DuplicateEventError')) {
+      const serviceOutput: SendQueryApiServiceOutput = { ...incomingRequest, workflowId, objectKey }
+      const serviceOutputResult = Result.makeSuccess(serviceOutput)
+      console.info(`${logCtx} exit success: from-error:`, {
+        publishEventResult,
+        serviceOutputResult,
+        incomingRequest,
+      })
+
+      return serviceOutputResult
+    }
+
+    console.error(`${logCtx} exit failure:`, { publishEventResult, incomingRequest })
+    return publishEventResult
+  }
+
+  /**
+   *
+   */
+  private validateInput(incomingRequest: IncomingSendQueryRequest): Success<void> | Failure<'InvalidArgumentsError'> {
+    const logCtx = 'SendQueryApiService.validateInput'
+    console.info(`${logCtx} init:`, { incomingRequest })
+
+    if (incomingRequest instanceof IncomingSendQueryRequest === false) {
+      const message = `Expected IncomingSendQueryRequest but got ${incomingRequest}`
+      const failure = Result.makeFailure('InvalidArgumentsError', message, false)
+      console.error(`${logCtx} exit failure:`, { failure, incomingRequest })
+      return failure
+    }
+
+    return Result.makeSuccess()
+  }
+
+  /**
+   *
+   */
+  private async publishWorkflowCreatedEvent(
+    incomingRequest: IncomingSendQueryRequest,
+    workflowId: string,
+    objectKey: string,
+  ): Promise<
+    Success<void> | Failure<'InvalidArgumentsError'> | Failure<'DuplicateEventError'> | Failure<'UnrecognizedError'>
+  > {
+    const logCtx = 'SendQueryApiService.publishWorkflowCreatedEvent'
+    console.info(`${logCtx} init:`, { incomingRequest })
+
+    const { promptEnhanceRounds, responseEnhanceRounds } = incomingRequest
+    const eventData: WorkflowCreatedEventData = {
+      workflowId,
+      objectKey,
+      promptEnhanceRounds,
+      responseEnhanceRounds,
+    }
+
+    const buildEventResult = WorkflowCreatedEvent.fromData(eventData)
+    if (Result.isFailure(buildEventResult)) {
+      console.error(`${logCtx} exit failure:`, { buildEventResult, eventData })
+      return buildEventResult
+    }
+
+    const event = buildEventResult.value
+    const publishEventResult = await this.eventStoreClient.publish(event)
+    Result.isFailure(publishEventResult)
+      ? console.error(`${logCtx} exit failure:`, { publishEventResult, event })
+      : console.info(`${logCtx} exit success:`, { publishEventResult, event })
+
+    return publishEventResult
+  }
+}

b2fa3b8c6a1a7d592976e1ae06920ac7c067f209
Matías Mutchinick <mutchinick@gmail.com>
2025-07-11 22:56:05 -0600

    Implement template service with clearly distinguishable actions and events

---
+++ infra/lib/template-service/processStepWorkerEntry.ts
@@ -0,0 +1,1 @@
+export { handler } from '../../../services/src/template-service/handlers/processStepWorker'
--- services/src/workflow/handlers/continueWorkflowWorker.spec.ts
+++
@@ -1,8 +0,0 @@
-import { handler } from './continueWorkflowWorker'
-
-// COMBAK: Can mock clients to assert the Controller is built as expected
-describe(`Workflow Service handlers continueWorkflowWorker tests`, () => {
-  it(`exports the handler function`, () => {
-    expect(typeof handler).toBe('function')
-  })
-})

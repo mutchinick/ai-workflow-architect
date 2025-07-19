ea616e51c45fc8f20313fe1e1cea468c3da8e4e7
Matías Mutchinick <mutchinick@gmail.com>
2025-07-18 19:50:18 -0600

    Refactor Lambda constructs to use log groups with infinite retention and destroy removal policy

--- infra/lib/template-service/CreateJobApiLambdaConstruct.ts
+++ infra/lib/template-service/CreateJobApiLambdaConstruct.ts
@@ -1,11 +1,13 @@
-import { Duration } from 'aws-cdk-lib'
+import { RemovalPolicy } from 'aws-cdk-lib'
 import { HttpApi, HttpMethod, PayloadFormatVersion } from 'aws-cdk-lib/aws-apigatewayv2'
 import { HttpLambdaIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations'
 import { Table } from 'aws-cdk-lib/aws-dynamodb'
 import { Runtime } from 'aws-cdk-lib/aws-lambda'
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
+import { LogGroup, RetentionDays } from 'aws-cdk-lib/aws-logs'
 import { Construct } from 'constructs'
 import { join } from 'path'
+import { settings } from '../settings'
 
 export interface ICreateJobApiLambdaConstructProps {
   httpApi: HttpApi
@@ -30,6 +32,13 @@ export class CreateJobApiLambdaConstruct extends Construct {
    */
   private createCreateJobApiLambdaFunction(scope: Construct, id: string, dynamoDbTable: Table): NodejsFunction {
     const lambdaFuncName = `${id}-Lambda`.slice(0, 64)
+
+    const logGroup = new LogGroup(scope, `${lambdaFuncName}LogGroup`, {
+      logGroupName: `/aws/lambda/${lambdaFuncName}`,
+      retention: RetentionDays.INFINITE,
+      removalPolicy: RemovalPolicy.DESTROY,
+    })
+
     const lambdaFunc = new NodejsFunction(scope, lambdaFuncName, {
       functionName: lambdaFuncName,
       runtime: Runtime.NODEJS_20_X,
@@ -38,7 +47,8 @@ export class CreateJobApiLambdaConstruct extends Construct {
       environment: {
         EVENT_STORE_TABLE_NAME: dynamoDbTable.tableName,
       },
-      timeout: Duration.seconds(10),
+      timeout: settings.Lambda.timeout,
+      logGroup,
     })
 
     dynamoDbTable.grantReadWriteData(lambdaFunc)
